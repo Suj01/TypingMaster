6d67e4fc0eab89e7f2d63daefc4c55a3e1a575ad
SUJU1998 <111004060+SUJU1998@users.noreply.github.com>
2024-05-30 01:24:04 +0530

    frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Center, Heading } from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+<Center><Heading>TypeMaster</Heading></Center>
+    </>
+  )
+}
+
+export default App
